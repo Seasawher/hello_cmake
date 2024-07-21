4696cc649afbd5b8daf296d8adc9844bb6a094d9
Seasawher <tellagoodstory@icloud.com>
2024-07-21 17:45:16 +0900

    commit

---
+++ hello.cpp
@@ -0,0 +1,6 @@
+#include <iostream> // 入出力ストリームライブラリをインクルードする
+#include "hello.hpp" // 自作のヘッダーファイル "hello.hpp" をインクルードする
+
+void hello() { // hello関数の定義を開始する
+    std::cout << "Hello!" << std::endl; // 標準出力に "Hello!" と改行を出力する
+}
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "hello.hpp" // "hello.hpp"というヘッダーファイルをインクルードする
+#include "good_morning.hpp"
+
+// エントリーポイントとなるmain関数の定義を開始する
+int main()
+{
+    hello(); // hello()関数を呼び出す
+    good_morning();
+}
